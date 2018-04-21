09492699ad8977136236650192cda5bbc46b9d69
exequielvial <38020602+exequielvial@users.noreply.github.com>
2018-04-21 01:10:03 -0300

    Add files via upload

---
+++ proyecto-grupo-04/Prueba Proyecto/Accesorio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_Proyecto
+{
+    public abstract class Accesorio
+    {
+
+        public double precio;
+        public int cantidad;
+
+        public Accesorio(double miPrecio, int miCantidad)
+        {
+            precio = miPrecio;
+            cantidad = miCantidad;
+        }
+        public virtual void Fragil()
+        {
+
+        }
+    }
+}
---
+++ proyecto-grupo-04/Prueba Proyecto/Notebook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_Proyecto
+{
+    public class Notebook : Accesorio
+    {
+        public Notebook(double precio, int cantidad) : base(precio, cantidad)
+        {
+
+        }
+        public override void Fragil()
+        {
+            Console.WriteLine("El usuario ha arrendado un computador, preocuparse que lo trate con cuidado");
+        }
+    }
+}
+
